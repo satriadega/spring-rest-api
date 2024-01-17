6715b66ebd4b44d2cf61b087027b8d00cc4b8f71
satriadega <satria.pandega@gmail.com>
2024-01-17 12:56:35 +0700

    unknown teritorry.

--- src/main/java/payroll/PayrollApplication.java
+++ src/main/java/payroll/PayrollApplication.java
@@ -3,11 +3,24 @@ package payroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import payroll.entity.Employee;
+
 @SpringBootApplication
 public class PayrollApplication {
 
+	public static void testing() {
+		Employee Bilbo = new Employee("Baggins", "burglar");
+		Employee Frodo = new Employee("Baggins", "burglar");
+
+		Bilbo.setId(1L);
+		Frodo.setId(1L);
+		System.out.println(Bilbo.equals(Frodo) + " 55555555555555");
+	}
+
 	public static void main(String... args) {
 		SpringApplication.run(PayrollApplication.class, args);
+
+		testing();
 	}
 
 }
