18bfd2309086a49afaaabd4320ba88eaa8955108
satriadega <satria.pandega@gmail.com>
2024-01-22 10:20:20 +0700

    errors.

--- src/main/java/payroll/PayrollApplication.java
+++ src/main/java/payroll/PayrollApplication.java
@@ -9,11 +9,11 @@ import payroll.entity.Employee;
 public class PayrollApplication {
 
 	public static void testing() {
-		Employee Bilbo = new Employee("Baggins", "burglar");
-		Employee Frodo = new Employee("Baggins", "burglar");
+		// Employee Bilbo = new Employee("Baggins", "burglar");
+		// Employee Frodo = new Employee("Baggins", "burglar");
 
-		Bilbo.setId(1L);
-		Frodo.setId(1L);
+		// Bilbo.setId(1L);
+		// Frodo.setId(1L);
 		// System.out.println(Bilbo.equals(Frodo) + " 55555555555555");
 		// System.out.println(Bilbo.hashCode());
 		// System.out.println(Bilbo.hashCode());
